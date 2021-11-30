d025436eeb88350083c4ffbd0d1d7e5d435ed1ef
iroDevs <ironegrolol@gamil.com>
2021-11-30 02:28:58 -0300

    buttons aparecendo e remover todos adcionado

--- crud-react/src/App.js
+++ crud-react/src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [cor, setCor] = useState("#5e72e4")
   const [nome,setNome] = useState("")
   const [quadrados,setQuadrados] = useState([])
+  const [mostraConfig,setConfig] = useState(false);
   function HandleClick(){
     let quadrado = {
       nome: nome,
@@ -12,6 +13,21 @@ function App() {
     }
     setQuadrados([quadrado , ...quadrados]);
   }
+
+  function deleteAll(){
+   
+    setQuadrados([]);
+  }
+  
+  function apareceConfig() {
+    setConfig(true)
+    
+  }
+ function desapareceConfig(param){
+   setConfig(false)
+ }
+
+
   return (
     <div className="Main">
       <p>React CRUD</p>
@@ -25,6 +41,7 @@ function App() {
           <input type="text" className="name-text" onChange={(e) => setNome(e.target.value)} placeholder="nome da caixa"></input>
           <input type="color" id="cor" value={cor} onChange={(e) => setCor(e.target.value)} ></input>
           <button type="button" onClick={HandleClick} id="button-subimit">Adcionar</button>
+          <button type="button" onClick={deleteAll} id="button-subimit">Remover Todos</button>
         </label>
       </form>
       <main>
@@ -34,11 +51,28 @@ function App() {
             let style = {
               backgroundColor:cor,
             }
-            return(
-              <div className="caixas" key={index} style={style} >
+            if(mostraConfig === true){
+               return(
+              <div  className="caixas" onMouseLeave={desapareceConfig} onMouseEnter={apareceConfig} key={index} style={style} >
                 <p className="caixas-name">{element.nome}</p>
+                <div className={`caixa-Buttons-out`}>
+                  <button className={`caixas-edit`} >EDIT</button>
+                  <button className={`caixas-delete`}>DELETE</button>
+                  </div>
               </div> 
             )
+
+            }
+            return(
+              <div  className="caixas" onMouseLeave={desapareceConfig} onMouseEnter={apareceConfig} key={index} style={style} >
+                <p className="caixas-name">{element.nome}</p>
+                <div className={`caixas-Buttons`}>
+                  <button className={`caixas-edit`} >EDIT</button>
+                  <button className={`caixas-delete`}>DELETE</button>
+                  </div>
+              </div> 
+            )
+
           })}
       </main>
     </div>
